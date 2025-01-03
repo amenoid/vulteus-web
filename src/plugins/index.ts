a6af76a591545efd1d1c58c76c8a3e917e7d7fee
Endrika Nanami <endrika.nanami@gmail.com>
2025-01-03 14:14:51 +0700

    Add Element Plus as UI Library

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -6,7 +6,8 @@
 import type { App } from 'vue'
 import router from '@/router'
 import pinia from '@/plugins/pinia.ts'
+import elementPlus from '@/plugins/element-plus'
 
 export function registerPlugins(app: App) {
-  app.use(router).use(pinia)
+  app.use(router).use(pinia).use(elementPlus)
 }
